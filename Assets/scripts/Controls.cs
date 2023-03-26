c9b6700e16aa5c19f1de63d6710030ef1ab0579f
NicolasMelet <nicolas.melet@epitech.eu>
2023-03-26 20:20:36 +0200

    Yay it works

--- Assets/scripts/Controls.cs
+++ Assets/scripts/Controls.cs
@@ -13,6 +13,8 @@ public class Controls : MonoBehaviour
     private int layerIndex;
     public Transform rayPointR;
     public Transform rayPointL;
+    public Transform ownR;
+    public Transform ownL;
     public Transform groundCheck;
     public LayerMask groundLayer;
     public grap_object player_grab;
@@ -39,22 +41,24 @@ public class Controls : MonoBehaviour
     private void FixedUpdate()
     {
         RaycastHit2D hitInfoR = Physics2D.Raycast(rayPointR.position, Vector3.right, rayDistance);
-        RaycastHit2D hitInfoL = Physics2D.Raycast(rayPointL.position, transform.right, rayDistance * -1);
+        RaycastHit2D hitInfoL = Physics2D.Raycast(rayPointL.position, Vector3.right, rayDistance * -1);
+        RaycastHit2D ownInfoR = Physics2D.Raycast(ownR.position, Vector3.right, rayDistance);
+        RaycastHit2D ownInfoL = Physics2D.Raycast(ownL.position, Vector3.right, rayDistance * -1);
 
         horizontal = Input.GetAxis("Horizontal") * speed;
         if (player_grab.is_holding)
         {
             if (is_grab == false)
             {
-                pantin.transform.position = new Vector2(transform.position.x, pantin.transform.position.y);
+                is_grab = true;
             }
-            is_grab = true;
             if ((hitInfoR.collider != null && hitInfoR.collider.gameObject.layer == layerIndex && horizontal > 0) ||
-                (hitInfoL.collider != null && hitInfoL.collider.gameObject.layer == layerIndex && horizontal < 0))
+                (hitInfoL.collider != null && hitInfoL.collider.gameObject.layer == layerIndex && horizontal < 0) ||
+                (ownInfoR.collider != null && ownInfoR.collider.gameObject.layer == layerIndex && horizontal > 0) ||
+                (ownInfoL.collider != null && ownInfoL.collider.gameObject.layer == layerIndex && horizontal < 0))
             {
                 horizontal = 0;
             }
-            pantin_rigid.velocity = new Vector2(horizontal, 0);
         }
         else
         {
@@ -68,8 +72,13 @@ public class Controls : MonoBehaviour
 
     private void LateUpdate()
     {
+        if (player_grab.is_holding)
+        {
+            pantin_rigid.velocity = new Vector2(horizontal, 0);
+        }
         rigid.velocity = new Vector2(horizontal, rigid.velocity.y);
     }
+
     private bool IsGrounded()
     {
         return Physics2D.OverlapCircle(groundCheck.position, 0.5f, groundLayer);
