32ef248d05dca0b1c17f4c104460cf190c029e04
Kanda09 <114816489+Kanda09@users.noreply.github.com>
2023-03-26 23:32:15 +0200

    [ADD] FINISH

--- Assets/scripts/ChangeScene.cs
+++ Assets/scripts/ChangeScene.cs
@@ -6,6 +6,7 @@ using UnityEngine.SceneManagement;
 public class ChangeScene : MonoBehaviour
 {
     public string levelToLoad;
+    public string ActuelLevel;
     private int layerIndex;
     
     void Start()
@@ -14,6 +15,14 @@ public class ChangeScene : MonoBehaviour
     }
     // Start is called before the first frame update
     
+    void Update()
+    {
+        if (Input.GetKeyDown("r"))
+        {
+            SceneManager.LoadScene(ActuelLevel);
+        }
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.layer == layerIndex)
