2f7a3f6b6849cd54b9bc3a8bf3c21a1adedbae82
Kanda09 <114816489+Kanda09@users.noreply.github.com>
2023-03-26 19:04:53 +0200

    [ADD] switch pivot

--- Assets/scripts/RotateFlip.cs
+++ Assets/scripts/RotateFlip.cs
@@ -4,25 +4,34 @@ using UnityEngine;
 
 public class RotateFlip : MonoBehaviour
 {
+    public Transform switchObj;
     public Transform customPivot;
     public bool rotateBool;
+    private int layerIndex;
     // Start is called before the first frame update
     void Start()
     {
+        layerIndex = LayerMask.NameToLayer("Perso");
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown("q") && rotateBool)
+        float rayDistance = 1;
+        RaycastHit2D hitInfo = Physics2D.Raycast(switchObj.position, Vector3.right, rayDistance);
+        
+        if (hitInfo.collider != null && hitInfo.collider.gameObject.layer == layerIndex)
         {
-            transform.RotateAround(customPivot.position, Vector3.forward, 90);
-            rotateBool = false;
-        }
-        else if (Input.GetKeyDown("q"))
-        {
-            transform.RotateAround(customPivot.position, Vector3.forward, -90);
-            rotateBool = true;
+            if (Input.GetKeyDown("q") && rotateBool)
+            {
+                transform.RotateAround(customPivot.position, Vector3.forward, 90);
+                rotateBool = false;
+            }
+            else if (Input.GetKeyDown("q"))
+            {
+                transform.RotateAround(customPivot.position, Vector3.forward, -90);
+                rotateBool = true;
+            }
         }
     }
 }
